0a5e936080de4ba9ff6eb33ed7ec9749e50d9a66
wolfrox71 <wolfrox71@gmail.com>
2023-06-01 15:40:19 +0100

    Init

---
+++ main.py
@@ -0,0 +1,63 @@
+import re
+import pyperclip
+import pyautogui
+from time import sleep
+
+def start_string(tag: str) -> str:
+    """returns the entered tag without the number part"""
+    
+    # find the last non int tag
+    return re.sub(r' \d+$', '', tag).strip()
+
+pyperclip.copy("")
+sleep(1)
+
+def copy():
+    # swap to chrome
+    pyautogui.keyDown("command")
+    pyautogui.keyDown("tab")
+    sleep(0.05)
+    pyautogui.keyUp("command")
+    pyautogui.keyUp("tab")
+
+    sleep(0.1)
+
+    # paste 
+    pyautogui.keyDown("command")
+    pyautogui.keyDown("v")
+    sleep(0.05)
+    pyautogui.keyUp("v")
+    pyautogui.keyUp("command")
+
+    sleep(0.1)
+    # enter the tag
+    pyautogui.press("Enter")
+
+    # swap to vs code
+    pyautogui.keyDown("command")
+    pyautogui.keyDown("tab")
+    sleep(0.05)
+    pyautogui.keyUp("command")
+    pyautogui.keyUp("tab")
+
+    # go onto the next item
+    #pyautogui.press("Enter")
+    #sleep(0.1)
+tags = set()
+with open("tag.txt", "r") as f:
+    lines = f.readlines()
+
+for line in lines:
+    if not line.startswith("?"):
+        continue
+    current = line.split("? ")[1]
+    tags.add((start_string(current)))
+
+pyperclip.copy("")
+for i, tag in enumerate(tags):
+    print(f"{i+1}/{len(tags)}): {tag}")
+    pyperclip.copy(tag)
+    sleep(0.15)
+    copy()
+    #sleep(0.15)
+    #input()
